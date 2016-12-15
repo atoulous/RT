d361078e80299551f544003bafce694889f38a9f
Jules BARBIER <jubarbie@e3r10p8.42.fr>
2016-12-15 15:20:51 +0100

    create new object files for news object structure compatibility

---
+++ Sources/plane-new.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   plane.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jubarbie <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/10/23 19:55:56 by jubarbie          #+#    #+#             */
+/*   Updated: 2016/12/15 14:51:18 by jubarbie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rt.h"
+
+void	plane(t_object *obj, t_ray *ray)
+{
+	t_v3d	tmp;
+	double	t;
+	double	det;
+
+	det = dot_v3d(O_DIR, ray->dir);
+	if (det < 0.0000001)
+	{
+		tmp = sub_v3d(O_POS, ray->pos);
+		t = dot_v3d(tmp, O_DIR) / det;
+		if (t > 0.0000001 && t < ray->dist)
+		{
+			ray->det = det;
+			ray->obj = obj;
+			ray->dist = t;
+			ray->norm = O_DIR;
+		}
+	}
+}
