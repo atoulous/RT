913a59da3c364565e43752819766f747ebaa7953
Jules <jubarbie@student.42.fr>
2017-02-06 16:05:36 +0100

    event matiere

--- Sources/menu_event.c
+++ Sources/menu_event.c
@@ -6,7 +6,7 @@
 /*   By: jubarbie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/04 14:29:49 by jubarbie          #+#    #+#             */
-/*   Updated: 2017/02/06 10:45:57 by jubarbie         ###   ########.fr       */
+/*   Updated: 2017/02/06 16:05:10 by jubarbie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,6 +53,18 @@ void	object_menu_event(t_env *e, int x, int y, t_object *obj)
 		(obj->r1 + step > 0) ? obj->r1 += step : 0;
 	else if (y > 408 && y < 430 && (type == 4 || type == 5))
 		((obj->r2 + step > 0 && type == 5) || type == 4) ? obj->r2 += step : 0;
+	else if (y > 484 && y < 501)
+		(obj->mat.diffuse + step > 0 && obj->mat.diffuse + step < 1) 
+			? obj->mat.diffuse += step : 0;
+	else if (y > 509 && y < 526)
+		(obj->mat.ambient + step > 0 && obj->mat.ambient + step < 1)
+			? obj->mat.ambient += step : 0;
+	else if (y > 533 && y < 551)
+		(obj->mat.shine + step > 0)
+			? obj->mat.shine += step : 0;
+	else if (y > 559 && y < 575)
+		(obj->mat.specular + step > 0 && obj->mat.specular + step < 1)
+			? obj->mat.specular += step : 0;
 	else
 		click = 0;
 	if (click)
@@ -112,7 +124,7 @@ void	right_menu_event(t_env *e, int x, int y)
 			&& y < 307)
 			color_selector(e, x - WIN_WIDTH + 231, y - 136);
 		if (x > WIN_WIDTH - 42 && x < WIN_WIDTH - 4 && y > 362
-			&& y < 430)
+			&& y < 630)
 			object_menu_event(e, x, y, (t_object *)(e->scene->obj_focus->content));
 		if (x > WIN_WIDTH - 231 && x < WIN_WIDTH - 9 && y > 444 && y < 470)
 			mat_menu_event(e, x, y, (t_object *)(e->scene->obj_focus->content));
